e9cea3eb93edd9f9728d654fc3dba74a935fa247
David G <david.a.grs@gmail.com>
2016-01-28 16:25:30 +0100

    Adding papi.h: PAPI wrappers

---
+++ papi.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <papi.h>
+
+#include <cstddef>
+#include <array>
+#include <string>
+
+namespace cpp
+{
+
+template <std::size_t _SizeT>
+static auto get_papi_event_names(const std::array<int, _SizeT>& events)
+{
+	std::array<std::string, _SizeT> ret;
+	for (std::size_t i = 0; i < ret.size(); ++i)
+	{
+			char event_code[PAPI_MAX_STR_LEN];
+			PAPI_event_code_to_name(events[i], event_code);
+			ret[i] = event_code;
+	}
+	return ret;
+}
+
+template <int... _EventsT>
+struct papi_wrapper
+{
+	static constexpr int events_count = sizeof...(_EventsT);
+	typedef std::array<long long, events_count> counters_type;
+
+	void start()
+	{
+		int ret;
+		if ((ret = ::PAPI_start_counters(const_cast<int*>(s_events.data()), events_count)) != PAPI_OK)
+			throw std::runtime_error(PAPI_strerror(ret));
+	}
+
+	void stop()
+	{
+		int ret;
+
+		if ((ret = PAPI_stop_counters(&m_counters[0], events_count)) != PAPI_OK)
+			throw std::runtime_error(PAPI_strerror(ret));
+	}
+
+	const counters_type& get_counters() const { return m_counters; }
+
+	template <int _EventIndexT>
+	long long get_counter() const { return m_counters[_EventIndexT]; }
+
+	template <int _EventIndexT>
+	static constexpr int get_event_type() { return s_events[_EventIndexT]; }
+
+	template <int _EventIndexT>
+	static const std::string& get_event_name() { return s_event_names[_EventIndexT]; }
+
+private:
+	static constexpr std::array<int, events_count> s_events = {{_EventsT...}};
+	static const std::array<std::string, events_count> s_event_names;
+
+	counters_type m_counters;
+};
+
+template <int... _EventsT> constexpr std::array<int, papi_wrapper<_EventsT...>::events_count> papi_wrapper<_EventsT...>::s_events;
+
+template <int... _EventsT> const std::array<std::string, papi_wrapper<_EventsT...>::events_count> papi_wrapper<_EventsT...>::s_event_names
+	= get_papi_event_names(papi_wrapper<_EventsT...>::s_events);
+
+typedef papi_wrapper<PAPI_L1_DCM, PAPI_L2_DCM, PAPI_L3_TCM> cache_profiler;
+typedef papi_wrapper<PAPI_TOT_INS, PAPI_TOT_CYC, PAPI_BR_MSP> instr_profiler;
+	
+}
+
