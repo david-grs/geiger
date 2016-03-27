4c9ed44e80f4a910ef7bda6aaacfe63b262c57eb
David G <david.a.grs@gmail.com>
2016-03-28 00:10:54 +0200

    Adding more basic unit tests

--- tests/basic.cpp
+++ tests/basic.cpp
@@ -58,3 +58,42 @@ TEST_F(Basic, TwoTests_RunTwice)
     ASSERT_EQ(4, m_on_test_complete_calls);
     ASSERT_EQ(2, m_on_complete_calls);
 }
+
+TEST_F(Basic, OneTest_CorrectName)
+{
+    m_suite.add("foo", []() { });
+    m_suite.on_test_complete([this](const std::string& name, const geiger::test_report&)
+    {
+        ASSERT_EQ("foo", name);
+
+    });
+    m_suite.run();
+}
+
+TEST_F(Basic, OneTest_NoPAPICounter)
+{
+    m_suite.add("foo", []() { });
+    m_suite.on_test_complete([this](const std::string&, const geiger::test_report& report)
+    {
+        ASSERT_EQ(0, report.papi_counters().size());
+
+    });
+    m_suite.run();
+}
+
+TEST_F(Basic, TwoTests_CorrectOrderReports)
+{
+    m_suite.add("foo", []() { });
+    m_suite.add("bar", []() { });
+    m_suite.on_test_complete([this](const std::string& name, const geiger::test_report&)
+    {
+        static int i = 0;
+
+        if (i++ == 0)
+            ASSERT_EQ("foo", name);
+        else
+            ASSERT_EQ("bar", name);
+
+    });
+    m_suite.run();
+}
