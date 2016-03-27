9136389df0ebe7d5d4a30538d26a1ef678364263
David G <david.a.grs@gmail.com>
2016-03-28 00:24:46 +0200

    chrono.cpp: usnig std::chrono instead of time.h stuff

--- geiger/chrono.cpp
+++ geiger/chrono.cpp
@@ -3,10 +3,6 @@
 #include <chrono>
 #include <thread>
 
-extern "C" {
-#include <time.h>
-}
-
 namespace geiger
 {
 
@@ -14,26 +10,24 @@ namespace detail
 {
 
 double tsc_freq_ghz = .0;
-}
 
-std::chrono::nanoseconds operator-(timespec end, timespec start)
-{
-    return std::chrono::nanoseconds((int64_t)((end.tv_sec - start.tv_sec) * 1e9 + end.tv_nsec - start.tv_nsec));
 }
 
 void init()
 {
-    timespec start, end;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+    using clock = std::chrono::high_resolution_clock;
+
+    auto start = clock::now();
 
     uint64_t rdtsc_start = detail::rdtsc();
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
     uint64_t rdtsc_end = detail::rdtsc();
 
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+    auto end = clock::now();
 
-    auto ns = end - start;
+    auto duration_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
     uint64_t cycles = rdtsc_end - rdtsc_start;
-    detail::tsc_freq_ghz = (double)cycles / ns.count();
+    detail::tsc_freq_ghz = (double)cycles / duration_ns.count();
 }
+
 }
